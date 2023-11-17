f5b35d13040b7119be250ec0eaf9f46b94a508df
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-17 18:59:11 +0100

    Fixed directory to make folder bug

--- makeDir.py
+++ makeDir.py
@@ -7,9 +7,12 @@ folders_in_dir = [
 ]
 
 folders_in_dir_last = folders_in_dir[len(folders_in_dir) - 1].replace("story", "")
-
 temp = int(folders_in_dir_last)
 for i in range(temp + 1, temp + 6):
     temp = temp + 1
-    os.mkdir("story" + str(temp).zfill(3))
+
+    new_story_folder = "./stories"
+    folder_name = f"story {str(temp).zfill(3)}"
+    path = os.path.join(new_story_folder, folder_name)
+    os.makedirs(path)
 
