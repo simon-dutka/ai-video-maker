e40ba8c10686493db869d7345c29bf0eeedb53d1
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-17 19:23:35 +0100

    Removed not needed variable

--- makeDir.py
+++ makeDir.py
@@ -11,9 +11,7 @@ temp = int(folders_in_dir_last)
 for i in range(temp + 1, temp + 6):
     temp = temp + 1
 
-    new_story_folder = "./stories"
-    folder_name = f"story {str(temp).zfill(3)}"
     folder_name = f"story{str(temp).zfill(4)}"
-    path = os.path.join(new_story_folder, folder_name)
+    path = os.path.join(dir_path, folder_name)
     os.makedirs(path)
 
