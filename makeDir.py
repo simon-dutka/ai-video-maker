28c4c2e46bd797f1e21f802b9985fe20c3a2773e
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-20 21:14:04 +0100

    Fixed checking if /stories is not empty bug
    -Added function

--- makeDir.py
+++ makeDir.py
@@ -1,21 +1,35 @@
 import os
 
-dir_path = './stories'
+def makeDire():
+    dir_path = './stories'
 
-if len(os.listdir(dir_path)) == 0:
-    path = os.path.join(dir_path, "story0001")
-    os.makedirs(os.path.join(dir_path, "story0001"))
+    if len(os.listdir(dir_path)) == 0:
+        path = os.path.join(dir_path, "story0001")
+        os.makedirs(os.path.join(dir_path, "story0001"))
 
-folders_in_dir = [
-    folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
-]
+    folders_in_dir = [
+        folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
+    ]
 
-folders_in_dir_last = folders_in_dir[len(folders_in_dir) - 1].replace("story", "")
-temp = int(folders_in_dir_last)
-for i in range(temp + 1, temp + 6):
-    temp = temp + 1
+    folders_in_dir_last = folders_in_dir[len(folders_in_dir) - 1].replace("story", "")
+    temp = int(folders_in_dir_last)
 
-    folder_name = f"story{str(temp).zfill(4)}"
-    path = os.path.join(dir_path, folder_name)
-    os.makedirs(path)
+    dir_to_upload_files = ""
 
+    # Allows get name of first created dir
+    num_of_loop = 0
+
+    for i in range(temp + 1, temp + 6):
+        num_of_loop += 1
+        temp = temp + 1
+        
+        folder_name = f"story{str(temp).zfill(4)}"
+
+        if num_of_loop == 1:
+            dir_to_upload_files = folder_name = f"story{str(temp).zfill(4)}"
+
+
+        path = os.path.join(dir_path, folder_name)
+        os.makedirs(path)
+
+# dir_to_upload_files
