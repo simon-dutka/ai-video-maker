fe5a1823cee2fc3f5f6f3f8e2bb318fc812a74a1
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-21 13:08:30 +0100

    Added exit option

--- controlPanel.py
+++ controlPanel.py
@@ -1,12 +1,16 @@
 import makeDir
 import inquirer
 
-choices = ['Make directory structure']
+choices = ['Make directory structure', 'Exit']
+isExit = False
 
-choice = inquirer.list_input("What's we gonna do today?",
-    choices=choices)
-choiceIndex = choices.index(choice)
-print(choiceIndex)
+while isExit == False:
+    choice = inquirer.list_input("What's we gonna do today?",
+        choices=choices)
 
-if choiceIndex == 0:
-    makeDir.makeDirectory()
+    choiceIndex = choices.index(choice)
+
+    if choiceIndex == 0:
+        makeDir.makeDirectory()
+    elif choiceIndex == 1:
+        isExit = True
