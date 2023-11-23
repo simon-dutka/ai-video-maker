bf67e4b90e60be8441b35ad172e4668dee820ff1
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-23 02:36:40 +0100

    Added makeFiles.py
    -Updating folder status from 'empty' to 'check'

---
+++ makeFiles.py
@@ -0,0 +1,14 @@
+# Get last (first of five) created folder
+# print(dir_to_upload_files)
+import os
+dir_path = './stories'
+
+folders_in_dir = [
+        folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
+]
+
+for i in range(len(folders_in_dir)):
+    if "empty" in folders_in_dir[i]:
+        folder_name = folders_in_dir[i]
+        new_folder_name = 'stories/' + folders_in_dir[i].replace('empty', 'check')
+        os.rename('stories/' + folders_in_dir[i], new_folder_name)
