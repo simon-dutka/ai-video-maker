aa2ac7ff2cb6ae5c9567d749e87280122c26c275
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-29 15:39:47 +0100

    Added function for generating storyxxxx.txt file

--- makeFiles.py
+++ makeFiles.py
@@ -1,12 +1,27 @@
 # Get last (first of five) created folder
 # print(dir_to_upload_files)
-import os
+import os, re
 dir_path = './stories'
 
 def make_files():
     folders_in_dir = [
             folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
     ]
+    
+    # print(os.listdir('./stories'))
+    print(folders_in_dir)
+    # story_file = open("stories/story", "w")
+    def create_story_file():
+        for directory in os.listdir('./stories'):
+            # print(directory)
+
+            if 'empty' in directory:
+                
+                file_name = int(re.sub('[a-z]', '', directory).replace(' - ', ''))
+                print(file_name)
+                story_file = open(f'stories/{directory}/story{str(file_name).zfill(4)}.txt', "w")
+
+    create_story_file()
 
     def change_dir_name(nameToChange, newName):
         for i in range(len(folders_in_dir)):
