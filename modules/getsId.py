2d3768a243d5509c3a9ed3f71645e932b41a9223
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-19 21:50:52 +0100

    Updated getsId.py
    Now function using argument to get reddit API

--- modules/getsId.py
+++ modules/getsId.py
@@ -1,18 +1,4 @@
-import praw
-import json
-
-with open ('.secret/keys.json', 'r') as f:
-    keys = json.load(f)
-
-reddit = praw.Reddit(
-    client_id=keys['CLIENT_ID'],
-    client_secret=keys['CLIENT_SECRET'],
-    user_agent="my user agent",
-)
-
-subreddit = reddit.subreddit("AmItheAsshole")
-
-def gets_id():
+def gets_id(subreddit):
     with open(f'data/stories_id.txt', "w") as stories_id:
         for post in subreddit.top(time_filter="day", limit=100):
             stories_id.write(post.id + '\n')
