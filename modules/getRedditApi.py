881db490e5dc22a22385caf4f57fe8f0aef7c0e4
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-20 14:14:18 +0100

    Updated getting reddit API

--- modules/getRedditApi.py
+++ modules/getRedditApi.py
@@ -3,11 +3,14 @@ import json
 with open ('.secret/keys.json', 'r') as f:
     keys = json.load(f)
 
-def get_reddit_api():
-    reddit = praw.Reddit(
+reddit = praw.Reddit(
         client_id=keys['CLIENT_ID'],
         client_secret=keys['CLIENT_SECRET'],
         user_agent="my user agent",
     )
 
+def get_reddit():
+    return reddit
+
+def get_subreddit():
     return reddit.subreddit("AmItheAsshole")
