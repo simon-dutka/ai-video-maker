aa30386d9014763a9067a36c2b54779aa6da2b96
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-22 10:14:58 +0100

    Changed if statement to switch
    -Added one option to control panel

--- controlPanel.py
+++ controlPanel.py
@@ -1,20 +1,27 @@
 import inquirer
-from modules import makeDir, makeFiles, getsId, getRedditApi
+from modules import makeDir, getsId, getRedditApi, changeReadyStatus, makeStoryFiles
 
-choices = ['Make directory structure', 'Make files structure', 'Get posts ID\'s', 'Exit']
+choices = ['Make directory structure', 'Make files structure', 'Get posts ID\'s', 'Set ready status', 'Exit']
 isExit = False
 
-while isExit == False:
+def get_choice():
     choice = inquirer.list_input("What's we gonna do today?",
         choices=choices)
+    return  choices.index(choice)
+    
 
-    choiceIndex = choices.index(choice)
+while isExit == False:
+    choiceIndex = get_choice()
 
-    if choiceIndex == 0:
-        makeDir.make_dir()
-    elif choiceIndex == 1:
-        makeFiles.make_files(getRedditApi.get_reddit())
-    elif choiceIndex == 2:
-        getsId.gets_id(getRedditApi.get_subreddit())
-    elif choiceIndex == 3:
-        isExit = True
+
+    match choiceIndex:
+        case 0:
+            makeDir.make_dir()
+        case 1:
+            makeStoryFiles.make_story_files(getRedditApi.get_reddit())
+        case 2:
+            getsId.gets_id(getRedditApi.get_subreddit())
+        case 3:
+            changeReadyStatus.change_ready_status()
+        case 4:
+            isExit = True
