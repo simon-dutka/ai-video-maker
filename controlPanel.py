58f1f43bd5ca29b72847504d5e0a5c79b1272b13
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-22 10:40:47 +0100

    Added new options to control panel
    -Imported new functions

--- controlPanel.py
+++ controlPanel.py
@@ -1,7 +1,7 @@
 import inquirer
-from modules import makeDir, getsId, getRedditApi, changeReadyStatus, makeStoryFiles
+from modules import makeDir, getsId, getRedditApi, changeReadyStatus, makeStoryFiles, makeVoiceFiles , makeVideoFiles
 
-choices = ['Make directory structure', 'Make files structure', 'Get posts ID\'s', 'Set ready status', 'Exit']
+choices = ['Make directory structure', 'Make story files', 'Make voice files', 'make video files' 'Get posts ID\'s', 'Set ready status', 'Exit']
 isExit = False
 
 def get_choice():
@@ -13,15 +13,18 @@ def get_choice():
 while isExit == False:
     choiceIndex = get_choice()
 
-
     match choiceIndex:
         case 0:
             makeDir.make_dir()
         case 1:
             makeStoryFiles.make_story_files(getRedditApi.get_reddit())
         case 2:
-            getsId.gets_id(getRedditApi.get_subreddit())
+            makeVoiceFiles.make_voice_files()
         case 3:
-            changeReadyStatus.change_ready_status()
+            makeVideoFiles.make_video_files()
         case 4:
+            getsId.gets_id(getRedditApi.get_subreddit())
+        case 5:
+            changeReadyStatus.change_ready_status()
+        case 6:
             isExit = True
