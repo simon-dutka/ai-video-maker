ff04b36d7086aade17b0db7b1e015b2d8f04660b
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-25 23:11:30 +0100

    Basics for generating voice files

--- modules/makeVoiceFiles.py
+++ modules/makeVoiceFiles.py
@@ -1,6 +1,34 @@
-import os, re
+import requests, json, os, re
 
-dir_path = './stories'
+with open ('.secret/keys.json', 'r') as f:
+    keys = json.load(f)
+
 
 def make_voice_files():
-    pass
+  dir_path = './stories'
+    
+  CHUNK_SIZE = 1024
+
+  voice_model_id = 'dPfEIZvhnol9pjmS6M7F'
+  url = "https://api.elevenlabs.io/v1/text-to-speech/" + voice_model_id
+
+  headers = {
+    "Accept": "audio/mpeg",
+    "Content-Type": "application/json",
+    "xi-api-key": keys['ELEVEN_LABS_API_KEY']
+  }
+
+  data = {
+    "text": "sample",
+    "model_id": "eleven_monolingual_v1",
+    "voice_settings": {
+      "stability": 0.5,
+      "similarity_boost": 0.5
+    }
+  }
+
+  response = requests.post(url, json=data, headers=headers)
+  with open('output.mp3', 'wb') as f:
+      for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+          if chunk:
+              f.write(chunk)
