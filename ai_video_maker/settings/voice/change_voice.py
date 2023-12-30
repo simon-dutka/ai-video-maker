5c490bf884ace2905e7ffb6fe9733e642cc7503f
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-30 10:38:11 +0100

    Updated change_voice()

--- ai_video_maker/settings/voice/change_voice.py
+++ ai_video_maker/settings/voice/change_voice.py
@@ -1,2 +1,16 @@
+from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
+
 def change_voice():
-    pass
+    choices = ['Set own voice', 'List of voices', 'Exit']
+    isExit = False
+
+    while isExit == False:
+        choiceIndex = get_inquirer_choice("Set voice to use", choices)
+
+        match choiceIndex:
+            case 0:
+                set_own_voice()
+            case 1:
+                list_of_voices()
+            case 2:
+                isExit = True
