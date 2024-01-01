2b58b4ae790f438502c43f87a9f87ff952ee1f6d
Simon Dutka <Szymon.dutka@outlook.com>
2024-01-01 18:20:59 +0100

    Updated import

--- ai_video_maker/settings/settings.py
+++ ai_video_maker/settings/settings.py
@@ -2,7 +2,7 @@ import sys
 from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
 
 from .subreddit.subreddit import subreddit
-from .voice.change_voice import voice
+from .voice.voice import voice
 
 def settings():
     choices = ['Subreddit', 'Voice', 'Exit']
