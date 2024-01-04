1e315694e96df056c660ce90dc2d54902990457f
Simon Dutka <Szymon.dutka@outlook.com>
2024-01-04 23:35:08 +0100

    Added function to set file to generate voice file

---
+++ ai_video_maker/voice_files/set_file_to_make_voice.py
@@ -0,0 +1,26 @@
+import os
+from  ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
+
+dir_path = 'stories'
+
+folders_in_dir = [
+    folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
+]
+
+choices = folders_in_dir
+choices.append('Exit')
+
+index_of_exit = len(choices) - 1
+
+def set_file_to_make_voice():
+
+    is_exit = False
+
+    while is_exit == False:
+        choiceIndex = get_inquirer_choice("Set file to make voice", choices)
+
+        if (choiceIndex == index_of_exit):
+            is_exit = True
+        else:
+            print(choices[choiceIndex])
+            # return choices[choiceIndex]
