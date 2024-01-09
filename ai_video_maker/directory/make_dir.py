7b252bbac8179fb207aa26c7ec39bd1d56e97f94
Simon Dutka <Szymon.dutka@outlook.com>
2024-01-09 23:30:14 +0100

    Getting num of creating directories from settings.json
    -Updated make_dir()

--- ai_video_maker/directory/make_dir.py
+++ ai_video_maker/directory/make_dir.py
@@ -1,13 +1,10 @@
 import os
 import re
+import json
 
 
 def make_dir():
     dir_path = "stories"
-
-    # If /story is empty make first dir
-    if len(os.listdir(dir_path)) == 1 or len(os.listdir(dir_path)) == 0:
-        os.makedirs(os.path.join(dir_path, "story0001 - Empty"))
 
     folders_in_dir = [
         folder
@@ -15,19 +12,27 @@ def make_dir():
         if os.path.isdir(os.path.join(dir_path, folder))
     ]
 
-    folders_in_dir_last = folders_in_dir[len(folders_in_dir) - 1]
-    folders_in_dir_last = int(
-        re.sub("[a-z]|[A-Z]", "", folders_in_dir_last).replace(" - ", "")
-    )
+    if len(folders_in_dir) == 0:
+        folders_in_dir_last = 0
+    else:
+        folders_in_dir_last = folders_in_dir[len(folders_in_dir) - 1]
+        folders_in_dir_last = int(
+            re.sub("[a-z]|[A-Z]", "", folders_in_dir_last).replace(" - ", "")
+        )
 
-    # Allows get name of first created dir
-    num_of_loop = 0
+    with open("settings/settings.json", "r") as f:
+        num_of_directories = json.load(f)["num_of_directories"]
 
-    for i in range(folders_in_dir_last, folders_in_dir_last + 1):
-        num_of_loop += 1
-        folders_in_dir_last = folders_in_dir_last + 1
+        end_of_range = (
+            num_of_directories
+            if folders_in_dir_last == 1
+            else folders_in_dir_last + num_of_directories
+        )
 
-        folder_name = f"story{folders_in_dir_last:04d} - Empty"
+        for i in range(folders_in_dir_last, end_of_range):
+            folders_in_dir_last = folders_in_dir_last + 1
 
-        path = os.path.join(dir_path, folder_name)
-        os.makedirs(path)
+            folder_name = f"story{folders_in_dir_last:04d} - Empty"
+
+            path = os.path.join(dir_path, folder_name)
+            os.makedirs(path)
