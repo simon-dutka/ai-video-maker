d6a7ee4bc0053ad484d547613c2309836ee03a60
Simon Dutka <Szymon.dutka@outlook.com>
2024-01-11 23:53:32 +0100

    Removed not needed lines

--- ai_video_maker/voice_files/make_voice_file.py
+++ ai_video_maker/voice_files/make_voice_file.py
@@ -1,46 +1,40 @@
 import requests, json, os, re
 
-with open ('.secret/keys.json', 'r') as f:
+with open(".secret/keys.json", "r") as f:
     keys = json.load(f)
 
 # my_file = open("stories/story0001 - empty/story0001.txt", "r")
 # textFile = my_file.read()
 
-dir_path = 'stories'
+dir_path = "stories"
 
 folders_in_dir = [
-    folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
+    folder
+    for folder in os.listdir(dir_path)
+    if os.path.isdir(os.path.join(dir_path, folder))
 ]
 
-print(folders_in_dir)
 
 def make_voice_file(story_file_name):
-  dir_path = './stories'
-    
-  CHUNK_SIZE = 1024
+    CHUNK_SIZE = 1024
 
-  voice_model_id = 'dPfEIZvhnol9pjmS6M7F'
-  url = "https://api.elevenlabs.io/v1/text-to-speech/" + voice_model_id
+    voice_model_id = "dPfEIZvhnol9pjmS6M7F"
+    url = "https://api.elevenlabs.io/v1/text-to-speech/" + voice_model_id
 
-  headers = {
-    "Accept": "audio/mpeg",
-    "Content-Type": "application/json",
-    "xi-api-key": keys['ELEVEN_LABS_API_KEY']
-  }
-
-  stories_id = {
-    "text": 'textFile',
-    "model_id": "eleven_monolingual_v1",
-    "voice_settings": {
-      "stability": 0.5,
-      "similarity_boost": 0.5
+    headers = {
+        "Accept": "audio/mpeg",
+        "Content-Type": "application/json",
+        "xi-api-key": keys["ELEVEN_LABS_API_KEY"],
     }
-  }
 
-  response = requests.post(url, json=stories_id, headers=headers)
-  with open('output.mp3', 'wb') as f:
-      for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-          if chunk:
-              f.write(chunk)
+    stories_id = {
+        "text": "textFile",
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {"stability": 0.5, "similarity_boost": 0.5},
+    }
 
-# make_voice_files()
+    response = requests.post(url, json=stories_id, headers=headers)
+    with open("output.mp3", "wb") as f:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
