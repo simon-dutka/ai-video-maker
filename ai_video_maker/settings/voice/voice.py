df20a259245881a8cb05bad2305ad2d555db4e46
Simon Dutka <Szymon.dutka@outlook.com>
2024-02-16 19:39:22 +0100

    Removed match case in voice.py

--- ai_video_maker/settings/voice/voice.py
+++ ai_video_maker/settings/voice/voice.py
@@ -4,19 +4,25 @@ from .set_own_voice import set_own_voice
 
 
 def voice():
-    choices = ["Set own voice", "List of voices to set", "List of using voices", "Exit"]
+    class exit_status:
+        def __init__(self, is_exit):
+            self.is_exit = is_exit
+
+        def change_exit_status(self):
+            self.is_exit = True
+
+    is_exit = exit_status(False)
+
+    choices = {
+        "Set own voice": set_own_voice,
+        "List of voices to set": list_of_voices_to_set,
+        "List of using voices": list_of_using_voices,
+        "Exit": is_exit.change_exit_status,
+    }
 
     isExit = False
 
-    while isExit is False:
-        choiceIndex = get_inquirer_choice("Voice settings", choices)
+    while is_exit.is_exit == False:
+        choiceIndex = get_inquirer_choice("Voice settings", list(choices.keys()))
 
-        match choiceIndex:
-            case 0:
-                set_own_voice()
-            case 1:
-                list_of_voices_to_set()
-            case 2:
-                list_of_using_voices()
-            case 3:
-                isExit = True
+        choices[list(choices)[choiceIndex]]()
