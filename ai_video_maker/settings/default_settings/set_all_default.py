8bb04b3e5f6a64b9767e6dac7b3be9602bf63f74
Simon Dutka <Szymon.dutka@outlook.com>
2024-02-19 17:04:31 +0100

    Updated names in set_all_default.py

--- ai_video_maker/settings/default_settings/set_all_default.py
+++ ai_video_maker/settings/default_settings/set_all_default.py
@@ -2,10 +2,8 @@ import json
 
 
 def set_all_default():
-    default_settings = ""
+    with open("settings/default_settings.json", "r") as default_settings_file:
+        default_settings = json.load(default_settings_file)
 
-    with open("settings/default_settings.json", "r") as default:
-        default_settings = json.load(default)
-
-    with open("settings/settings.json", "w") as settings:
-        json.dump(default_settings, settings)
+    with open("settings/settings.json", "w") as settings_file:
+        json.dump(default_settings, settings_file, indent=4)
