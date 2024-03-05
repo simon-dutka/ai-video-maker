c36d29a035fe0ca7e0036b8b98762cff62d20ef3
Simon Dutka <Szymon.dutka@outlook.com>
2024-03-05 23:39:50 +0100

    Implemented set_choice() in directories.py

--- ai_video_maker/settings/directories/directories.py
+++ ai_video_maker/settings/directories/directories.py
@@ -1,26 +1,11 @@
-from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
-
 from .num_of_directories import num_of_directories
+from ai_video_maker.choice.set_choice import set_choice
 
 
 def directories():
-    class exit_status:
-        def __init__(self, is_exit):
-            self.is_exit = is_exit
-
-        def change_exit_status(self):
-            self.is_exit = True
-
-    is_exit = exit_status(False)
-
     choices = {
         "Change number of creating directories": num_of_directories,
-        "Exit": is_exit.change_exit_status,
+        "Exit": None,
     }
 
-    is_exit = False
-
-    while is_exit.is_exit == False:
-        choiceIndex = get_inquirer_choice("Settings", list(choices.keys()))
-
-        choices[list(choices)[choiceIndex]]()
+    set_choice(choices, "Settings")
