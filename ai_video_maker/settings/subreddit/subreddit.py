dc64841c6697530395f3ec221f27bb252a8925d9
Simon Dutka <Szymon.dutka@outlook.com>
2024-03-05 23:42:05 +0100

    Implemented set_choice() in subreddit.py

--- ai_video_maker/settings/subreddit/subreddit.py
+++ ai_video_maker/settings/subreddit/subreddit.py
@@ -1,28 +1,14 @@
 from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
 from .set_own_subreddit import set_own_subreddit
 from .show_using_subreddits import show_using_subreddits
+from ai_video_maker.choice.set_choice import set_choice
 
 
 def subreddit():
-    class exit_status:
-        def __init__(self, is_exit):
-            self.is_exit = is_exit
-
-        def change_exit_status(self):
-            self.is_exit = True
-
-    is_exit = exit_status(False)
-
     choices = {
         "Set own subreddit": set_own_subreddit,
-        "List of most popular subreddits to set": list_of_subreddits,
+        # "List of most popular subreddits to set": list_of_subreddits,
         "Show using subreddits": show_using_subreddits,
-        "Exit": is_exit.change_exit_status,
+        "Exit": None,
     }
-
-    is_exit = False
-
-    while is_exit == False:
-        choiceIndex = get_inquirer_choice("Subreddit settings", list(choices.keys()))
-
-        choices[list(choices)[choiceIndex]]()
+    set_choice(choices, "Subreddit settings")
