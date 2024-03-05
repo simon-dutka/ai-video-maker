3d7adfe0fd7133ca9dbfda81087377c775db81f0
Simon Dutka <Szymon.dutka@outlook.com>
2024-03-05 22:13:19 +0100

    implemented set_choice() in manual.py

--- ai_video_maker/manual/manual.py
+++ ai_video_maker/manual/manual.py
@@ -1,10 +1,12 @@
 from rich.console import Console
 from rich.markdown import Markdown
-from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
-
+from ai_video_maker.choice.set_choice import set_choice
 
 choices = {
-    "How to change voice": "ai_video_maker/manual/manuals/how_to_change_voice.md",
+    "How to change voice": lambda: display_manual(
+        "ai_video_maker/manual/manuals/how_to_change_voice.md"
+    ),
+    "Exit": None,
 }
 
 
@@ -13,20 +15,5 @@ def display_manual(file_path):
         Console().print(Markdown(file.read()))
 
 
-class exit_status:
-    def __init__(self, is_exit):
-        self.is_exit = is_exit
-
-    def change_exit_status(self):
-        self.is_exit = True
-
-
-is_exit = exit_status(False)
-
-
 def manual():
-    while is_exit.is_exit == False:
-        choiceIndex = get_inquirer_choice("Manual", list(choices.keys()))
-
-        display_manual(choices[list(choices)[choiceIndex]])
-        # ToDo: Add exit function
+    set_choice(choices, "Manual")
