9388737247cf9c4b874a4f999abb214dbafabd90
Simon Dutka <Szymon.dutka@outlook.com>
2024-03-05 23:14:00 +0100

    Implemented set_choice() in settings.py

--- ai_video_maker/settings/settings.py
+++ ai_video_maker/settings/settings.py
@@ -1,30 +1,17 @@
-from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
-
 from .directories.directories import directories
 from .subreddit.subreddit import subreddit
 from .voice.voice import voice
 from .default_settings.set_default_settings import set_default_settings
+from ai_video_maker.choice.set_choice import set_choice
 
 
 def settings():
-    class exit_status:
-        def __init__(self, is_exit):
-            self.is_exit = is_exit
-
-        def change_exit_status(self):
-            self.is_exit = True
-
-    is_exit = exit_status(False)
-
     choices = {
         "Directories": directories,
         "Subreddit": subreddit,
         "Voice": voice,
         "Set default settings": set_default_settings,
-        "Exit": is_exit.change_exit_status,
+        "Exit": None,
     }
 
-    while is_exit.is_exit == False:
-        choiceIndex = get_inquirer_choice("Settings", list(choices.keys()))
-
-        choices[list(choices)[choiceIndex]]()
+    set_choice(choices, "Settings")
