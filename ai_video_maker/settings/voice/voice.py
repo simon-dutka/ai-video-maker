14212a000cf12cd06ec9e3c07e6e469cfd489243
Simon Dutka <Szymon.dutka@outlook.com>
2024-03-05 23:48:28 +0100

    Implemented set_choice() in voice.py
    -Commented not completed functions

--- ai_video_maker/settings/voice/voice.py
+++ ai_video_maker/settings/voice/voice.py
@@ -1,28 +1,13 @@
-from ai_video_maker.inquirer_files.get_inquirer_choice import get_inquirer_choice
-
 from .set_own_voice import set_own_voice
+from ai_video_maker.choice.set_choice import set_choice
 
 
 def voice():
-    class exit_status:
-        def __init__(self, is_exit):
-            self.is_exit = is_exit
-
-        def change_exit_status(self):
-            self.is_exit = True
-
-    is_exit = exit_status(False)
-
     choices = {
         "Set own voice": set_own_voice,
-        "List of voices to set": list_of_voices_to_set,
-        "List of using voices": list_of_using_voices,
-        "Exit": is_exit.change_exit_status,
+        # "List of voices to set": list_of_voices_to_set,
+        # "List of using voices": list_of_using_voices,
+        "Exit": None,
     }
 
-    is_exit = False
-
-    while is_exit.is_exit == False:
-        choiceIndex = get_inquirer_choice("Voice settings", list(choices.keys()))
-
-        choices[list(choices)[choiceIndex]]()
+    set_choice(choices, "Voice settings")
