0842b8175f1fcc8baa75ff02bdc5c69af64cf61b
Simon Dutka <Szymon.dutka@outlook.com>
2024-05-30 18:07:55 +0200

    Updated create_info.py file

--- ai_video_maker/info/create_info.py
+++ ai_video_maker/info/create_info.py
@@ -1,8 +1,15 @@
 import json
+import os
 
 
-def create_info(file_path, directory):
-    data = {"directory_name": directory, "status": "empty"}
+def create_info(
+    directory_path,
+    directory_name,
+):
+    data = {"directory_name": directory_name, "status": "empty"}
 
-    with open(file_path, "w") as info_file:
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(directory_path), exist_ok=True)
+
+    with open(directory_path, "w") as info_file:
         json.dump(data, info_file, indent=4)
