346ff116906cfa277f00a9101f10285f3ea4d380
Simon Dutka <Szymon.dutka@outlook.com>
2024-06-13 18:25:47 +0200

    Deleted gets_id.py file

--- ai_video_maker/api/gets_id.py
+++
@@ -1,4 +0,0 @@
-def gets_id(subreddit):
-    with open("stories_id/stories_id.txt", "w") as stories_id:
-        for post in subreddit.top(time_filter="day", limit=100):
-            stories_id.write(post.id + "\n")
