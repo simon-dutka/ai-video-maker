acb4b07425489dfa97edbcaf1c99751b539fb729
Simon Dutka <Szymon.dutka@outlook.com>
2024-07-10 00:21:46 +0200

    Added checking info file instead of directory name

--- ai_video_maker/voice_files/set_file_to_make_voice.py
+++ ai_video_maker/voice_files/set_file_to_make_voice.py
@@ -1,4 +1,5 @@
 import os
+import json
 from .make_voice_file import make_voice_file
 from ai_video_maker.choice import set_choice
 
@@ -7,7 +8,11 @@ def set_file_to_make_voice():
     choices = {}
 
     for directory in os.listdir("./stories"):
-        if "- Ready to make audio" in directory:
-            choices[directory] = lambda: make_voice_file(directory)
+        if os.path.isdir(f"stories/{directory}"):
+            with open(f"stories/{directory}/info.json", "r", encoding="utf-8") as file:
+                data = json.load(file)
 
+                if isinstance(data, dict):
+                    if data.get("status") == "Ready to make audio":
+                        choices[directory] = lambda: make_voice_file(directory)
     return set_choice("Set file to make voice", choices)
