261c7d482b46b02462d9a4e4c4ec7ef2dc5a0618
Simon Dutka <Szymon.dutka@outlook.com>
2024-07-16 03:02:17 +0200

    Added checking info file

--- ai_video_maker/video_files/set_file_to_make_video.py
+++ ai_video_maker/video_files/set_file_to_make_video.py
@@ -1,4 +1,5 @@
 import os
+import json
 from .make_video_files import make_video_files
 from ai_video_maker.choice import set_choice
 
@@ -7,7 +8,12 @@ def set_file_to_make_video():
     choices = {}
 
     for directory in os.listdir("./stories"):
-        if "- Ready to make video" in directory:
-            choices[directory] = lambda: make_video_files(directory)
+        if os.path.isdir(f"stories/{directory}"):
+            with open(f"stories/{directory}/info.json", "r", encoding="utf-8") as file:
+                data = json.load(file)
+
+                if isinstance(data, dict):
+                    if "Ready to make video" in directory:
+                        choices[directory] = lambda: make_video_files(directory)
 
     set_choice(choices, "Set file to make video")
