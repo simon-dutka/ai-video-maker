269313e36e53682ccbb821c5f33e92574c429a32
Simon Dutka <Szymon.dutka@outlook.com>
2024-08-04 22:51:15 +0200

    Added function to updating backgrounds_info.json file

--- ai_video_maker/download_background_video/download_background_video.py
+++ ai_video_maker/download_background_video/download_background_video.py
@@ -1,5 +1,27 @@
 import os
+import json
 from yt_dlp import YoutubeDL
+from moviepy.editor import VideoFileClip
+
+
+def update_backgrounds_info(video_name):
+    video_path = f"./background_videos/{video_name}"
+    video = VideoFileClip(video_path)
+
+    background_info_file_path = "./background_videos/backgrounds_info.json"
+
+    with open(background_info_file_path, "r") as json_file:
+        json_info_data = json.load(json_file)
+
+    new_background_info = {
+        video_name: "never used",
+        "background-length": round(video.duration / 60, 1),
+    }
+
+    json_info_data["available backgrounds"].append(new_background_info)
+
+    with open(background_info_file_path, "w") as json_file:
+        json.dump(json_info_data, json_file, indent=4)
 
 
 def download_background_video(url):
@@ -15,6 +37,8 @@ def download_background_video(url):
 
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
+
+            update_backgrounds_info(f"{video_name}.mp4")
 
     except:
         print("Error")
