310d47adb3ae90232dee06ad18792395dcc4659a
Simon Dutka <Szymon.dutka@outlook.com>
2024-08-07 02:41:54 +0200

    Updated changing statuses

--- ai_video_maker/statuses/change_status.py
+++ ai_video_maker/statuses/change_status.py
@@ -1,4 +1,5 @@
 import os
+import json
 from ai_video_maker.choice import set_choice
 
 dir_path = "./stories"
@@ -7,5 +8,11 @@ if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
 
-def change_status(file_to_change_status):
-    print("works")
+def change_status(file_to_change_status, new_status):
+    with open(f"./stories/{file_to_change_status}/info.json", "r") as file:
+        data = json.load(file)
+
+    data["status"] = new_status
+
+    with open(f"./stories/{file_to_change_status}/info.json", "w") as file:
+        json.dump(data, file, indent=4)
