57ba1db49db4f2f1d7a2715f032bfa4c317dc34d
Simon Dutka <Szymon.dutka@outlook.com>
2024-08-10 01:52:37 +0200

    Updated get_reddit function

--- ai_video_maker/api/get_reddit_api.py
+++ ai_video_maker/api/get_reddit_api.py
@@ -1,17 +1,17 @@
 import praw
 import json
 
-with open(".secret/keys.json", "r") as f:
-    keys = json.load(f)
-
-reddit = praw.Reddit(
-    client_id=keys["REDDIT_CLIENT_ID"],
-    client_secret=keys["REDDIT_CLIENT_SECRET"],
-    user_agent="my user agent",
-)
-
 
 def get_reddit():
+    with open(".secret/keys.json", "r") as file:
+        keys = json.load(f)
+
+    reddit = praw.Reddit(
+        client_id=keys["REDDIT_CLIENT_ID"],
+        client_secret=keys["REDDIT_CLIENT_SECRET"],
+        user_agent="my user agent",
+    )
+
     return reddit
 
 
